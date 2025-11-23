123563fb26d002e4ae07a37d00991dcadde8dacb
João Victor <jvictor.sg@outlook.com>
2025-11-23 19:00:21 -0300

    Adicionado o metodo que salva os dados dos usuários

--- src/main/java/com/joao/usuario/infrastructure/entity/Usuario.java
+++ src/main/java/com/joao/usuario/infrastructure/entity/Usuario.java
@@ -1,10 +1,7 @@
 package com.joao.usuario.infrastructure.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -16,7 +13,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Builder
 @Entity
 @Table(name = "usuario")
 
