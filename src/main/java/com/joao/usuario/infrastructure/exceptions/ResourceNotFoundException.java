57c2393094e9a0705ccc0dae360be76964800418
João Victor <jvictor.sg@outlook.com>
2025-11-23 21:36:33 -0300

    Adcionado os metodos de login, get e  delete

---
+++ src/main/java/com/joao/usuario/infrastructure/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.joao.usuario.infrastructure.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String mensagem){
+        super(mensagem);
+    }
+
+    public ResourceNotFoundException(String mensagem, Throwable throwable){
+        super(mensagem, throwable);
+    }
+}
